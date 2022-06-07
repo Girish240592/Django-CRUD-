75046d026923b965cf01ed1a7c4aa83c065290d6
Girsh240592 <girishrode2405@gmail.com>
2022-06-07 14:12:40 +0530

    Added Employee Model

--- CRUD/app/admin.py
+++ CRUD/app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Employees
+from .models import *
 
 # Register your models here.
 
+# admin.site.register(Employees)
+
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = ('name','email','address','phone')
+
 admin.site.register(Employees)
+
